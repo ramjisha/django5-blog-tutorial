a44a958b25f43f3e44488cd696fbe084b06f45e0
ramjisha <crramji004@gmail.com>
2024-08-22 16:23:00 +0530

    last modification

--- myapp/urls.py
+++ myapp/urls.py
@@ -22,4 +22,6 @@ handler404 = 'myapp.views.custom_page_not_found'
 urlpatterns = [
     path("", include("blog.urls")),
     path('admin/', admin.site.urls),
+    path("", include("blog.urls")),
+    path("", include("blog.urls")),
 ]
